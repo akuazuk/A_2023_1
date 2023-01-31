317b144ca123d0844039a84a43a4ac57bf9414c1
Pavel <akuazuk@gmail.com>
2023-01-30 18:05:53 -0700

    Updated

--- OpenAI_app_1/distribution.py
+++ OpenAI_app_1/distribution.py
@@ -1,39 +1,37 @@
+import numpy as np
+import scipy.stats as stats
 import streamlit as st
 import matplotlib.pyplot as plt
-import numpy as np
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_page_config(page_background_color='black')
 
-def plot_distribution(distribution_type, a, b):
-    if distribution_type == 'Normal':
-        mu = 0
-        sigma = 1
-        s = np.random.normal(mu, sigma, 1000)
-        count, bins, ignored = plt.hist(s, 30, density=True)
-        plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - 
-mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
-        plt.title('Normal Distribution')
-        plt.xlabel('X')
-        plt.ylabel('Probability')
-    else:
-        x = np.linspace(0, 1, 1000)
-        beta = np.random.beta(a, b, 1000)
-        count, bins, ignored = plt.hist(beta, 30, density=True)
-        y = (x**(a-1)) * ((1-x)**(b-1)) / (b ** (a+b-1) / ((a-1)*(b-1)))
-        plt.plot(x, y, linewidth=2, color='r')
-        plt.title('Beta Distribution')
-        plt.xlabel('X')
-        plt.ylabel('Probability')
-    return plt
+# Define beta distribution
+a = st.slider("a", 0.1, 10.0, 1.0)
+b = st.slider("b", 0.1, 10.0, 1.0)
 
-distribution_type = st.sidebar.selectbox("Select Distribution Type", 
-["Normal", "Beta"])
+x = np.linspace(0, 1, 100)
+y = stats.beta.pdf(x, a, b)
 
-if distribution_type == 'Beta':
-    a = st.sidebar.slider("a", 0, 10, 1)
-    b = st.sidebar.slider("b", 0, 10, 1)
+# Plot the beta distribution
+fig, ax = plt.subplots(figsize=(10, 5), facecolor='black')
+ax.plot(x, y, 'red', lw=2)
+ax.set_facecolor('black')
+st.pyplot(fig)
 
-fig, ax = plt.subplots()
-plot = plot_distribution(distribution_type, a, b)
+# Show shaded area under the curve
+start, end = st.slider("Probability range", 0, 1, (0.1, 0.9))
+
+# Calculate the area under the curve in the given range
+x_range = np.linspace(start, end, 100)
+y_range = stats.beta.pdf(x_range, a, b)
+area = np.trapz(y_range, x_range)
+
+# Plot the shaded area
+fig, ax = plt.subplots(figsize=(10, 5), facecolor='black')
+ax.fill_between(x_range, y_range, 0, color='red', alpha=0.5)
+ax.plot(x, y, 'red', lw=2)
+ax.set_facecolor('black')
+ax.annotate(f"P({start:.2f} < X < {end:.2f}) = {area:.2f}", (start + end) 
+/ 2, 0.05, color='red', fontsize=14)
 st.pyplot(fig)
 
