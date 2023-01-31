ce3a30707719f923f6a69ea00b5bd510b4510540
Pavel <akuazuk@gmail.com>
2023-01-30 22:32:31 -0700

    Updated

--- OpenAI_app_1/distribution.py
+++ OpenAI_app_1/distribution.py
@@ -5,30 +5,30 @@ import scipy.stats as stt
 
 st.title("Beta Distribution Plotter")
 
-alpha = st.slider("Alpha", 0.1, 5.0, 2.0, 0.1)
-beta = st.slider("Beta", 0.1, 5.0, 2.0, 0.1)
+alpha = st.slider("Alpha", 1, 10, 5, 1)
+beta = st.slider("Beta", 1, 10, 5, 1)
 
 beta_dist = stt.beta(alpha, beta)
 
 x = np.linspace(beta_dist.ppf(0.01), beta_dist.ppf(0.99), 100)
 
-st.write("## Beta PDF")
+st.write("### Beta PDF")
 st.write("### Alpha =", alpha)
 st.write("### Beta =", beta)
 
-plt.plot(x, beta_dist.pdf(x), 'k-', lw=2, label='frozen pdf')
+plt.plot(x, beta_dist.pdf(x), 'k-', lw=2, color='red', label='frozen pdf')
 
-st.write("## Beta CDF")
+st.write("### Beta CDF")
 st.write("### Alpha =", alpha)
 st.write("### Beta =", beta)
 
-plt.plot(x, beta_dist.cdf(x), 'k--', lw=2, label='frozen cdf')
+plt.plot(x, beta_dist.cdf(x), 'k--', lw=2, color='green', label='frozen cdf')
 
-st.write("## Beta SF (Survival Function)")
+st.write("### Beta SF (Survival Function)")
 st.write("### Alpha =", alpha)
 st.write("### Beta =", beta)
 
-plt.plot(x, beta_dist.sf(x), 'k:', lw=2, label='frozen sf')
+plt.plot(x, beta_dist.sf(x), 'k:', lw=2, color='blue', label='frozen sf')
 
 plt.legend()
 st.pyplot()
