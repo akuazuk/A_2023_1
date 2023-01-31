4dee0db0cbf3762e883f29b0b9fbd6a764131f5d
Pavel <akuazuk@gmail.com>
2023-01-30 22:58:00 -0700

    Updated

--- OpenAI_app_1/distribution.py
+++ OpenAI_app_1/distribution.py
@@ -5,17 +5,17 @@ import scipy.stats as stt
 
 st.title("Beta Distribution Plotter")
 
-alpha = st.slider("Alpha", 1, 100, 50, 1)
-beta = st.slider("Beta", 1, 100, 50, 1)
+alpha = st.slider("Alpha", 1, 50, 25, 1)
+beta = st.slider("Beta", 1, 50, 25, 1)
 
 beta_dist = stt.beta(alpha, beta)
 
-x = np.linspace(beta_dist.ppf(0.01), beta_dist.ppf(1.0), 100)
+x = np.linspace(beta_dist.ppf(0), beta_dist.ppf(1), 100)
 
 #st.write("### Alpha =", alpha)
 #st.write("### Beta =", beta)
 
-plt.plot(x, beta_dist.pdf(x), 'k', lw=2, color='red', label='pdf')
+plt.plot(x, beta_dist.pdf(x), 'k', lw=2, color='orange', label='pdf')
 
 
 plt.plot(x, beta_dist.cdf(x), 'k', lw=2, color='lightgreen', label='cdf')
