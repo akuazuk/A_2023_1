38d43badda14c43da3f5114c685d3cae68574527
Pavel <akuazuk@gmail.com>
2023-01-30 20:56:44 -0700

    Updated

--- OpenAI_app_1/distribution.py
+++ OpenAI_app_1/distribution.py
@@ -1,26 +1,23 @@
-import random
 import streamlit as st
 
-st.title("Two Dice Rolling Simulator")
+def calculate_probability(prior_a, prior_b, likelihood_a, likelihood_b, evidence):
+    posterior_a = (prior_a * likelihood_a) / evidence
+    posterior_b = (prior_b * likelihood_b) / evidence
+    return posterior_a, posterior_b
 
-def simulate_rolls(num_rolls):
-    num_sixes = 0
-    for i in range(num_rolls):
-        dice1 = random.randint(1, 6)
-        dice2 = random.randint(1, 6)
-        if dice1 == 6 and dice2 == 6:
-            num_sixes += 1
-    return num_sixes
+def main():
+    st.title("Bayes Theorem Probability Calculator")
+    st.write("Enter prior probabilities and likelihood ratios:")
+    prior_a = st.number_input("Prior probability of Hypothesis A", 0.5, min_value=0, max_value=1)
+    prior_b = st.number_input("Prior probability of Hypothesis B", 0.5, min_value=0, max_value=1)
+    likelihood_a = st.number_input("Likelihood ratio of Hypothesis A", 1, min_value=0)
+    likelihood_b = st.number_input("Likelihood ratio of Hypothesis B", 1, min_value=0)
+    evidence = prior_a * likelihood_a + prior_b * likelihood_b
+    posterior_a, posterior_b = calculate_probability(prior_a, prior_b, likelihood_a, likelihood_b, evidence)
+    st.write("Posterior probabilities:")
+    st.write("Posterior probability of Hypothesis A: ", posterior_a)
+    st.write("Posterior probability of Hypothesis B: ", posterior_b)
 
-# Initialize the number of rolls and runs
-num_rolls = st.slider("Number of rolls per simulation", min_value=1, max_value=1000, value=100, step=1)
-num_runs = st.slider("Number of simulations", min_value=1, max_value=1000, value=100, step=1)
-
-if st.button("Roll Dice"):
-    result = simulate_rolls(num_rolls)
-    total_sixes = 0
-    for i in range(num_runs):
-        total_sixes += simulate_rolls(num_rolls)
-    st.write("After", num_rolls * num_runs, "rolls, the average number of sixes is:", total_sixes/num_runs)
-    st.write("The a posteriori probability of getting two sixes is:", total_sixes/(num_rolls * num_runs))
+if __name__ == '__main__':
+    main()
 
