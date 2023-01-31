69b5361fec10bf16cfe8df7680664eb112572539
Pavel <akuazuk@gmail.com>
2023-01-31 09:04:27 -0700

    Updated

--- OpenAI_app_1/distribution.py
+++ OpenAI_app_1/distribution.py
@@ -1,15 +1,33 @@
+
 import streamlit as st
-import numpy as np
-from scipy.stats import beta
+from streamlit_cropper import st_cropper
+from PIL import Image
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-def plot_beta_pdf(a, b, x, y_min, y_max):
-    x_grid = np.linspace(x.min(), x.max(), 250)
-    pdf = beta.pdf(x_grid, a, b)
+# Upload an image and set some options for demo purposes
+st.header("Cropper Demo")
+img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'])
+realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
+box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
+aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
+aspect_dict = {
+    "1:1": (1, 1),
+    "16:9": (16, 9),
+    "4:3": (4, 3),
+    "2:3": (2, 3),
+    "Free": None
+}
+aspect_ratio = aspect_dict[aspect_choice]
+
+if img_file:
+    img = Image.open(img_file)
+    if not realtime_update:
+        st.write("Double click to save crop")
+    # Get a cropped image from the frontend
+    cropped_img = st_cropper(img, realtime_update=realtime_update, box_color=box_color,
+                                aspect_ratio=aspect_ratio)
     
-    return st.area_chart(pdf, x_grid, y_min=y_min, y_max=y_max)
-
-a = st.slider("a", 0.1, 10.0, 2.0, step=0.1)
-b = st.slider("b", 0.1, 10.0, 2.0, step=0.1)
-x = np.linspace(0, 1, 100)
-
-plot_beta_pdf(a, b, x, 0, 5)
+    # Manipulate cropped image at will
+    st.write("Preview")
+    _ = cropped_img.thumbnail((150,150))
+    st.image(cropped_img)
