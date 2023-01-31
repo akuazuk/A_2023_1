c8d506f3235caabc53b97d48f66c45679b438ee1
Pavel <akuazuk@gmail.com>
2023-01-30 23:33:46 -0700

    Updated

--- OpenAI_app_1/distribution.py
+++ OpenAI_app_1/distribution.py
@@ -3,9 +3,10 @@ import numpy as np
 from scipy.stats import beta
 
 def plot_beta_pdf(a, b, x, y_min, y_max):
-x_grid = np.linspace(x.min(), x.max(), 250)
-pdf = beta.pdf(x_grid, a, b)
-return st.area_chart(pdf, x_grid, y_min=y_min, y_max=y_max)
+    x_grid = np.linspace(x.min(), x.max(), 250)
+    pdf = beta.pdf(x_grid, a, b)
+    
+    return st.area_chart(pdf, x_grid, y_min=y_min, y_max=y_max)
 
 a = st.slider("a", 0.1, 10.0, 2.0, step=0.1)
 b = st.slider("b", 0.1, 10.0, 2.0, step=0.1)
