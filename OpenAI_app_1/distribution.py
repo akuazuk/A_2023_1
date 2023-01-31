a49fe88cc199cee8e841b4b3e904a3b08c359f04
Pavel <akuazuk@gmail.com>
2023-01-30 22:25:07 -0700

    Updated

--- OpenAI_app_1/distribution.py
+++ OpenAI_app_1/distribution.py
@@ -1,24 +1,35 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import beta
+import scipy.stats as stt
 
-st.title("Beta Distribution Probability Density Function Visualization")
+st.title("Beta Distribution Plotter")
 
-alpha, beta = st.slider("alpha", 1, 10, 1), st.slider("beta", 1, 10, 1)
+alpha = st.slider("Alpha", 0.1, 5.0, 2.0, 0.1)
+beta = st.slider("Beta", 0.1, 5.0, 2.0, 0.1)
 
-dist = beta(alpha, beta)
+beta_dist = stt.beta(alpha, beta)
 
-x = np.linspace(0, 1, 100)
-y = dist.pdf(x)
+x = np.linspace(beta_dist.ppf(0.01), beta_dist.ppf(0.99), 100)
 
-st.write("The probability density function of the Beta distribution with alpha =", alpha, "and beta =", beta, "is:")
+st.write("## Beta PDF")
+st.write("### Alpha =", alpha)
+st.write("### Beta =", beta)
 
-plt.plot(x, y)
-plt.fill_between(x, y, 0, alpha=0.2)
-plt.title("Beta Distribution PDF")
-plt.xlabel("x")
-plt.ylabel("pdf(x)")
+plt.plot(x, beta_dist.pdf(x), 'k-', lw=2, label='frozen pdf')
 
+st.write("## Beta CDF")
+st.write("### Alpha =", alpha)
+st.write("### Beta =", beta)
+
+plt.plot(x, beta_dist.cdf(x), 'k--', lw=2, label='frozen cdf')
+
+st.write("## Beta SF (Survival Function)")
+st.write("### Alpha =", alpha)
+st.write("### Beta =", beta)
+
+plt.plot(x, beta_dist.sf(x), 'k:', lw=2, label='frozen sf')
+
+plt.legend()
 st.pyplot()
 
